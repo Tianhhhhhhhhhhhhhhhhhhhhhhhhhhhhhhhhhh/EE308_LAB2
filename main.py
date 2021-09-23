54d03242cbc1916176f84a6599cd7a152a13ddc9
Tianhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <91210927+Tianhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2021-09-23 14:17:48 +0800

    Update main.py
    Implement the advanced requirement

--- main.py
+++ main.py
@@ -9,20 +9,45 @@ keywords = ["auto", "break", "case", "char", "const", "continue", "default", "do
             "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static",
             "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"]
 keywordNum = 0
+switchNum = 0
+caseNum = []
+tempNum = 0
 
-# Basic requirement implement
+# Advanced  requirement implement
+with open(filePath, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.replace("(", " ")
+        line = line.replace(")", " ")
+        line = line.replace(";", " ")
+        line = line.replace("{", " ")
+        line = line.replace("}", " ")
+        line = line.replace(":", " ")
+        words = line.split()
+        for word in words:
+            # count keywords
+            if word in keywords:
+                keywordNum += 1
+
+            # count switch and case
+            if word == "switch":
+                switchNum += 1
+            if word == "case":
+                tempNum += 1
+            if word == "default":
+                caseNum.append(tempNum)
+                tempNum = 0
+
+# Output
 if completionLevel == "1":
-    with open(filePath, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.replace("(", " ")
-            line = line.replace(")", " ")
-            line = line.replace(";", " ")
-            line = line.replace("{", " ")
-            line = line.replace("}", " ")
-            line = line.replace(":", " ")
-            words = line.split()
-            for word in words:
-                if word in keywords:
-                    keywordNum += 1
     print("total num: ", keywordNum)
+elif completionLevel == "2":
+    print("total num: ", keywordNum)
+    print("switch num: ", switchNum)
+    print("case num: ", caseNum)
+elif completionLevel == "3":
+    print("Coming soon")
+elif completionLevel == "4":
+    print("Coming soon")
+else:
+    print("Input the completion level between 1~4")
